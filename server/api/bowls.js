fb5ab3d13d28a2b3b65cb2db11c7063b6c24fa5d
Alexandra Ash <alexandraleah@gmail.com>
2018-07-25 22:47:44 -0400

    working on bowl routes

--- server/api/bowls.js
+++ server/api/bowls.js
@@ -20,3 +20,27 @@ router.post('/', async (req, res, next) => {
     next(err)
   }
 })
+
+//some middleware to find the campus and send it on to the next route or handle the error if there is one
+router.use('/:id', async (req, res, next) => {
+  try {
+    const bowl = await Bowl.find({
+      where: {
+        id: req.params.id
+      }
+    })
+    //if the promise returns null because no bowls are found
+    if (!bowl) {
+      const error = new Error('There is no bowl with id of' + req.params.id)
+      //send an error
+      error.status = 404
+      return next(error)
+    }
+    //add the bowl to the request before sending it along
+    req.bowl = bowl
+    next()
+    //catch any other errors
+  } catch (error) {
+    next(error)
+  }
+})
