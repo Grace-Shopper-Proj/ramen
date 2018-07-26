fd6777443cd1c9661941b4a26c3307bab78ef314
Siwing Tsoi <siwing@gmail.com>
2018-07-26 19:55:09 -0400

    (feature) create a skeleton of the AccountManagement view

--- client/app.js
+++ client/app.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
+
 import OrderPage from './components/OrderPage'
+import AccountManagement from './AccountManagement'
 import {Navbar} from './components'
 import Routes from './routes'
 
 const App = () => {
   return (
     <div>
-      <OrderPage />
       <Navbar />
       <Routes />
+      <Switch>
+        <Route exact path="/home" component={OrderPage} />
+        <Route exact path="/account" component={AccountManagement} />
+        <Redirect to="/home" />
+      </Switch>
     </div>
   )
 }
