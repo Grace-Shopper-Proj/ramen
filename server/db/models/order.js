aedfeed19ab4e37095ea6a04d8b145d3393e86fc
Alexandra Ash <alexandraleah@gmail.com>
2018-07-26 11:16:09 -0400

    feature(models) added get price method to order model

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+const {Bowl} = require('./index')
 
 const Order = db.define('orders', {
   //true if order is still in progress (it is still a cart.) False if order is complete
@@ -9,5 +10,20 @@ const Order = db.define('orders', {
     defaultValue: false
   }
 })
+
+//method to calculate price
+Order.prototype.getPrice = async function() {
+  //find all bowls with this order
+  try {
+    const bowls = await Bowl.findAll({
+      where: {orderId: this.id}
+    })
+    bowls.reduce((totalPrice, currentBowl) => totalPrice + currentBowl.price, 0)
+  } catch (error) {
+    console.log(error)
+  }
+
+  //add price
+}
 
 module.exports = Order
