d66bb394cdbda877fd7f58fec4878e107b28a8aa
Alexandra Ash <alexandraleah@gmail.com>
2018-09-02 16:25:33 -0400

    fix(current bowl): button shows disabled when ingredients not chosen

--- client/components/CurrentBowl.js
+++ client/components/CurrentBowl.js
@@ -51,18 +51,26 @@ export default function CurrentBowl(props) {
         </p>
         <h5>Total Price: </h5>
         <p>${currentPrice}</p>
-        <button
-          className="btn btn-danger btn-block"
-          type="submit"
-          disabled={
-            !currentBowl.selectedBroth ||
-            !currentBowl.selectedNoodles ||
-            !currentBowl.selectedProtein
-          }
-          onClick={props.submitBowl}
-        >
-          Add To Cart
-        </button>
+        {currentBowl.selectedBroth.title &&
+        currentBowl.selectedNoodles.title &&
+        currentBowl.selectedProtein.title ? (
+          <button
+            className="btn btn-danger btn-block"
+            type="submit"
+            onClick={props.submitBowl}
+          >
+            Add To Cart
+          </button>
+        ) : (
+          <button
+            className="btn btn-danger btn-block"
+            type="submit"
+            disabled
+            onClick={props.submitBowl}
+          >
+            Add To Cart
+          </button>
+        )}
       </div>
     </div>
   )
