b36aa8903f8c7d1667d70ffd70cd06566fe31e43
Alexandra Ash <alexandraleah@gmail.com>
2018-09-03 17:41:50 -0400

    layout(footer): adds footer

---
+++ client/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = function(props) {
+  return (
+    <footer className="text-center">
+      <p>
+        Copyright &copy; 2018 Listing. All rights reserved | By Alexandra Ash,
+        Kathy Pangtay, Cindy Song and Ivy Tsoi
+      </p>
+      <p>
+        <a href="https://github.com/Grace-Shopper-Proj/ramen">
+          <i className="fab fa-github" />
+        </a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
